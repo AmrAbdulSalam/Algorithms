ac5f24b48d78fa1a9ccacdee6e6200940c67142f
mohammad-nayef <awladmohammad20@gmail.com>
2023-07-12 10:26:40 +0300

    conflict for sure now

--- Dummy file.cpp
+++ Dummy file.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     cout << "I hate Front-End!\n"; //and js
-    cout << "fr tho why isn't there a conflict??\n";
+    cout << "fr tho why isn't there a conflict (4th attempt)??\n";
     //this gotta (second attempt) make a conflict (edit)
     //where is the conflict
  }
